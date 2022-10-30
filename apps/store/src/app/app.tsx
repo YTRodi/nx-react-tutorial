12c2dc4a7d69d14abee7dcec754dbedd1b4bb4e7
YTRodi <yagorodi@gmail.com>
2022-10-30 12:50:18 -0300

    commiting to test affected

--- apps/store/src/app/app.tsx
+++ apps/store/src/app/app.tsx
@@ -1,12 +1,17 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.scss';
-import NxWelcome from './nx-welcome';
+import { Banner } from '@myorg/common-ui';
+import { exampleProducts } from '@myorg/products';
 
 export function App() {
   return (
     <>
-      <NxWelcome title="store" />
-      <div />
+      <Banner text="Welcome to the store!" />
+      <ul>
+        {exampleProducts.map((product) => (
+          <li key={product.id}>
+            <strong>{product.name}</strong> Price: {product.price}
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
